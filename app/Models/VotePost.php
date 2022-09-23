ff71aa5f7248904c68627ee3b2b0ff680a0790c2
ibrahimrangkuti <rangkutyibrahim765@gmail.com>
2022-09-23 10:23:27 +0700

    Membuat Model Post, Category, Event, EventParticipant, LikePost, VotePost

---
+++ app/Models/VotePost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VotePost extends Model
+{
+    use HasFactory;
+}
